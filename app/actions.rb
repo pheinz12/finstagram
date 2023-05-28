a967d3bc46ea3c5f9a1aeac20d11883335c6976f
Paola Heinz <paolaheinz@outlook.com>
2023-05-28 05:42:09 +0000

    all changes

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,6 @@
-def humanized_time_ago(minute_num)
-  if minute_num >= 60
-    "#{minute_num / 60} hours ago"
-  else
-    "#{minute_num} minutes ago"
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
   end
 end
 
@@ -25,26 +23,84 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
 end
 
+get '/login' do
+  erb(:login)
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
 get '/finstagram_posts/new' do
-    erb(:"finstagram_posts/new")
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
 end
 
 post '/finstagram_posts' do
   photo_url = params[:photo_url]
 
-  @finstagram_post = FinstagramPost.new({ photo_url: photo_url })
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
 
   if @finstagram_post.save
     redirect(to('/'))
   else
-
-    @finstagram_post.errors.full_messages.inspect
+    erb(:"finstagram_posts/new")
   end
 end
+
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])   # find the finstagram post with the ID from the URL
+  erb(:"finstagram_posts/show")               # render app/views/finstagram_posts/show.erb
+end
+
+post '/comments' do
+  # point values from params to variables
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  # instantiate a comment with those values & assign the comment to the `current_user`
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  # save the comment
+  comment.save
+
+  # `redirect` back to wherever we came from
+  redirect(back)
+end
   
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
